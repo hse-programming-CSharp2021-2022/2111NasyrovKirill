c6a97748dc4b39bb808ae830417c22298966716a
KirillNasyrov <90870970+KirillNasyrov@users.noreply.github.com>
2021-11-04 09:45:41 +0300

    распределил проекты по папкам

---
+++ module2/seminar1/homework/ex2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public Point(double x, double y) { X = x; Y = y; }
+            public Point() : this(0, 0) { } // конструктор умолчания
+            public double Ro
+            {
+                get
+                {
+                    return X * X + Y * Y;
+                }
+            }
+
+            public double Fi
+            {
+                get
+                {
+                    if (X > 0 & Y >= 0) return Math.Atan(Y / X);
+                    else if (X > 0 & Y < 0) return Math.Atan(Y / X) + 2 * Math.PI;
+                    else if (X < 0) return Math.Atan(Y / X) + Math.PI;
+                    else if (X == 0 & Y > 0) return Math.Atan(Math.PI / 2);
+                    else if (X == 0 & Y < 0) return Math.Atan(3 * Math.PI / 2);
+                    else return 0;
+                }
+            }
+
+            public string PointData
+            {   // СВОЙСТВО 
+                get
+                {
+                    string maket = "X = {0:F2}; Y = {1:F2}; Ro = {2:F2}; Fi = {3:F2} ";
+                    return string.Format(maket, X, Y, Ro, Fi);
+                }
+            }
+
+            public double Lenght
+            {
+                get
+                {
+                    return Math.Sqrt(X * X + Y * Y);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Point a, b, c;
+            a = new Point(3, 4);
+            Console.WriteLine(a.PointData);
+            b = new Point(0, 3);
+            Console.WriteLine(b.PointData);
+            c = new Point();
+            double x = 0, y = 0;
+            do
+            {
+                Console.Write("x = ");
+                double.TryParse(Console.ReadLine(), out x);
+                Console.Write("y = ");
+                double.TryParse(Console.ReadLine(), out y);
+                c.X = x; c.Y = y;
+                double[] mas = new double[3];
+                mas[0] = a.Lenght; mas[1] = b.Lenght; mas[2] = c.Lenght;
+                Array.Sort(mas);
+
+                if (mas[0] == a.Lenght & mas[1] == b.Lenght & mas[2] == c.Lenght)
+                {
+                    Console.WriteLine(a.PointData);
+                    Console.WriteLine(b.PointData);
+                    Console.WriteLine(c.PointData);
+                }
+
+                if (mas[0] == a.Lenght & mas[1] == c.Lenght & mas[2] == b.Lenght)
+                {
+                    Console.WriteLine(a.PointData);
+                    Console.WriteLine(c.PointData);
+                    Console.WriteLine(b.PointData);
+                }
+
+                if (mas[0] == b.Lenght & mas[1] == a.Lenght & mas[2] == c.Lenght)
+                {
+                    Console.WriteLine(b.PointData);
+                    Console.WriteLine(a.PointData);
+                    Console.WriteLine(c.PointData);
+                }
+
+                if (mas[0] == b.Lenght & mas[1] == c.Lenght & mas[2] == a.Lenght)
+                {
+                    Console.WriteLine(b.PointData);
+                    Console.WriteLine(c.PointData);
+                    Console.WriteLine(a.PointData);
+                }
+
+                if (mas[0] == c.Lenght & mas[1] == a.Lenght & mas[2] == b.Lenght)
+                {
+                    Console.WriteLine(c.PointData);
+                    Console.WriteLine(a.PointData);
+                    Console.WriteLine(b.PointData);
+                }
+
+                if (mas[0] == c.Lenght & mas[1] == b.Lenght & mas[2] == a.Lenght)
+                {
+                    Console.WriteLine(c.PointData);
+                    Console.WriteLine(b.PointData);
+                    Console.WriteLine(a.PointData);
+                }
+            } while (x != 0 | y != 0);
+        }
+
+
+    }
+}
+
---
+++ module2/seminar1/homework/ex3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex3
+{
+    class Polygon
+    {
+        int number;
+        double radius;
+        public double Per
+        {
+            get
+            {
+                double R = radius / Math.Cos(Math.PI / number);
+                double a = 2 * R * Math.Sin(Math.PI / number);
+                return number * a;
+            }
+        }
+        public double Sq
+        {
+            get
+            {
+                return 0.5 * Per * radius;
+            }
+        }
+        public Polygon(int number = 0, double radius = 0)
+        {
+            this.number = number; this.radius = radius;
+        }
+        public string PoligonData()
+        {
+            return "Стороны = " + number + "\n" + "радиус  вписаной окружности= " + radius + "\n" + "периметр = " + Per + "\n" + "площадь = " + Sq;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Polygon> list = new List<Polygon>();
+            Console.WriteLine("Ввод количества сторон и радиуса фигуры");
+            int.TryParse(Console.ReadLine(), out int num);
+            int.TryParse(Console.ReadLine(), out int rad);
+            Console.WriteLine();
+            double max = 0;
+            double min = 100000000;
+            while (num != 0 | rad != 0)
+            {
+                Polygon a = new Polygon(num, rad);
+                if (a.Sq > max) max = a.Sq;
+                if (a.Sq < min) min = a.Sq;
+                list.Add(a);
+                Console.WriteLine("Ввод количества сторон и радиуса фигуры");
+                int.TryParse(Console.ReadLine(), out num);
+                int.TryParse(Console.ReadLine(), out rad);
+                Console.WriteLine();
+            }
+            Polygon[] mas = list.ToArray();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i].Sq == max)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(mas[i].PoligonData());
+                }
+                else if (mas[i].Sq == min)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(mas[i].PoligonData());
+                }
+                else
+                {
+                    Console.ResetColor();
+                    Console.WriteLine(mas[i].PoligonData());
+                }
+            }
+
+
+
+
+        }
+    }
+}
