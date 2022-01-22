407ffb0bd6fd8d1e93035c0146fbfe6226581b84
KirillNasyrov <90870970+KirillNasyrov@users.noreply.github.com>
2022-01-22 23:10:04 +0300

    -

---
+++ module3/homework31/homework31_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework31_1
+{
+    class Program
+    {
+        delegate int Cast(double number);
+        static void Main(string[] args)
+        {
+            Cast cast1, cast2;
+            cast1 = delegate(double number)
+            {
+                return (int) number % 2 == 0 ? (int) number : (int) number + 1;
+            };
+            cast2 = (number) => ((int) number).ToString().Length;
+            Console.WriteLine(cast1(23.3));
+            Console.WriteLine(cast2(1323.3));
+            Console.WriteLine(cast2(132343.3));
+            Console.WriteLine(cast2(1324));
+        }
+    }
+}
