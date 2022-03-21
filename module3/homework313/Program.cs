aa29ec9c93a6e57d559b3613e017e057e3ddb86f
KirillNasyrov <90870970+KirillNasyrov@users.noreply.github.com>
2022-03-21 18:38:01 +0300

    -

---
+++ module3/homework313/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+
+namespace homework313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fi = new();
+            TriangleNums tr = new();
+            int m = 10;
+            int[] arr = new int[m];
+            int i = 0;
+            foreach (int numb in fi.NameEnumerator(m))
+            {
+                arr[i] = numb;
+                Console.WriteLine(numb);
+                i++;
+            }
+
+            Console.WriteLine("*******");
+
+            i = 0;
+            foreach (int numb in tr.NameUnumerator(m))
+            {
+                arr[i] += numb;
+                Console.WriteLine(numb);
+                i++;
+            }
+            Console.WriteLine("*******");
+            foreach (int n in  arr)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        class TriangleNums
+        {
+            public IEnumerable NameUnumerator(int max)
+            {
+                for (int i = 0; i < max; i++)
+                {
+                    yield return (int)(i / 2.0 * (i * (i + 1)));
+                }
+                
+            }
+        }
+
+        class Fibonacci
+        {
+            private int f0 = 1;
+            private int f1 = 0;
+            public IEnumerable NameEnumerator(int max)
+            {
+                for (int i = 0; i < max; i++)
+                {
+                    int t = f0 + f1;
+                    f0 = f1;
+                    f1 = t;
+                    yield return t;
+                }
+            }
+        }
+    }
+}
